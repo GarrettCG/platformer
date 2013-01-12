8ebe23ba56967fffc459d2ea807e226679ef8d91
skelly <odin8472@gmail.com>
2013-01-12 01:23:21 -0500

    second commit

---
+++ Lwprog/src/display/LevelMaker.java
@@ -0,0 +1,11 @@
+package display;
+
+import framework.Framework;
+ 
+public class LevelMaker{
+ //honestly this class does nothing but declare main and get the ball rolling
+    public static void main(String[] argv) {
+		Framework.CURRENT_TUTORIAL_DATAPATH = "/Lwprog/data/";
+		new	FragChangeColor04().start();
+    }
+}
